b5d67861b6e3478f4dbe5407f5186e9b139ee792
Tomas Brejl <tomas.brejl@gmail.com>
2020-07-07 19:12:25 -0700

    started base class

---
+++ Classes-Practice/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classes_Practice
+{
+    class Pokemon
+    {
+        public string name;
+        public string type;
+        public string weaknesses;
+        public string resistance;
+        public int hp;
+
+        public void greeting() {
+            Console.WriteLine(name);
+        }
+
+    }
+
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git gud");
+        }
+
+    }
+}
