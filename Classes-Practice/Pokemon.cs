82d2c497409c92a75163bab28b234e7cf3ecdd06
Tomas Brejl <tomas.brejl@gmail.com>
2020-07-07 19:24:48 -0700

    finished first class

--- Classes-Practice/Pokemon.cs
+++ Classes-Practice/Pokemon.cs
@@ -5,21 +5,40 @@ namespace Classes_Practice
     class Pokemon
     {
         public string name;
-        public string type;
+        public string pokeType;
         public string weaknesses;
         public string resistance;
         public int hp;
 
         public void greeting() {
-            Console.WriteLine(name);
+            Console.WriteLine(name+"!");
         }
 
     }
 
-    class Program {
+    class Water: Pokemon {
+        public string weakness1;
+        public string weakness2;
+
+        public Water () {
+            pokeType = "water";
+            weakness1 = "grass";
+            weakness2 = "electric";
+        }
+    }
+
+    class Pokedex {
         static void Main(string[] args)
         {
-            Console.WriteLine("Git gud");
+            Water squirtle = new Water();
+            squirtle.name = "Squirtle";
+            squirtle.hp = 20;
+
+            Console.WriteLine("Name: "+squirtle.name);
+            Console.WriteLine("Type: "+squirtle.pokeType);
+            Console.Write("Greeting: ");
+            squirtle.greeting();
+            Console.WriteLine("Weaknesses: "+squirtle.weakness1+" and "+squirtle.weakness2);
         }
 
     }
